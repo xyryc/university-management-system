93b85ff83e9905e7b8f3d963a6f6ce2e475fdce1
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-09-26 23:20:37 +0600

    update

---
+++ admin_login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<center><br><br>
+	<h3>Admin Login Page</h3><br>
+	<form action="" method="POST">
+		Email: <input type="text" name="email" required><br><br>
+		Password: <input type="Password" name="Password" required><br><br>
+		<input type="submit" name="student_login" value="Submit" required>
+	</form>
+	</center>
+</body>
+</html>
