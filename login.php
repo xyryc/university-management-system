29101b7be7d3f3dff02ef3407f93dae181fec9b2
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-09-28 18:32:12 +0600

    update

--- login.php
+++ login.php
@@ -2,9 +2,7 @@
 <html>
 <head>
 	<title>Login Page</title>
-	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
-  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
-  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 	<center><br><br>
--- student_login.php
+++ student_login.php
@@ -2,9 +2,6 @@
 <html>
 <head>
 	<title>Student Login</title>
-	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
-  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
-  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<center><br><br>
