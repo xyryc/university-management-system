71ccc7e9b79a982b067d899889995263c3bc1baa
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-11-04 03:27:33 +0600

    admin_update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,17 +2,43 @@
 <html>
 <head>
 	<title>Admin Dashboard</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="shortcut icon" type="image/jpg" href="images/icon.jpg">
+  <link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" type="image/jpg" href="images/icon.jpg">
 </head>
 <body>
-	<div class="login-box">
-  <h1>Admin Dashboard</h1>
-   <p>
-        Work in Progress..
-  </p>
- 
-    </div>
+	<div class="header">
+    <center>University Management System</center>
+</div>
+
+<br><br>
+
+<div id="left_side" >
+  <form action="" method="post">
+    <table>
+      <tr>
+        <td>
+          <input type="submit" name="search_student" value="Search Student"  
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" name="edit_student" value="Edit Student"  
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" name="add_new_student" value="Add New Student"  
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" name="delete_student" value="Delete Student"  
+        </td>
+      </tr>
+    </table>
+  </form>
+</div>
+
 
 </body>
 </html>
