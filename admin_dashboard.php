fd24e3eb41540427fa06d362168457b37d5622d3
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-11-04 07:40:30 +0600

    admin_update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -5,34 +5,42 @@
   <link rel="stylesheet" href="css/style.css">
 	<link rel="shortcut icon" type="image/jpg" href="images/icon.jpg">
 </head>
+
+<?php
+  session_start();
+?>
+
 <body>
 	<div class="header">
-    <center>University Management System</center>
+    <center><strong>Admin Dashboard &nbsp&nbsp&nbsp&nbsp</strong><br>Email: <?php echo $_SESSION['email']; ?> &nbsp&nbsp
+      Name: <?php echo $_SESSION['name']; ?> 
+      <a href="logout.php">Logout</a>
+    </center>
 </div>
 
 <br><br>
 
-<div id="left_side" >
+<div class="left_side">
   <form action="" method="post">
     <table>
       <tr>
         <td>
-          <input type="submit" name="search_student" value="Search Student"  
+          <input type="submit" name="search_student" value="Search Student">
         </td>
       </tr>
       <tr>
         <td>
-          <input type="submit" name="edit_student" value="Edit Student"  
+          <input type="submit" name="edit_student" value="Edit Student">
         </td>
       </tr>
       <tr>
         <td>
-          <input type="submit" name="add_new_student" value="Add New Student"  
+          <input type="submit" name="add_new_student" value="Add New Student">  
         </td>
       </tr>
       <tr>
         <td>
-          <input type="submit" name="delete_student" value="Delete Student"  
+          <input type="submit" name="delete_student" value="Delete Student">  
         </td>
       </tr>
     </table>
--- admin_login.php
+++ admin_login.php
@@ -23,6 +23,7 @@
   </form>
 
 <?php
+    session_start();
     if(isset($_POST['submit'])){
         $connection = mysqli_connect("localhost", "root", "");
         $db = mysqli_select_db($connection, "sdponedb");
@@ -31,6 +32,8 @@
         while($row = mysqli_fetch_assoc($query_run)){
             if($row['email'] == $_POST['email']){
                 if($row['password'] == $_POST['password']){
+                    $_SESSION['email'] = $row['email'];
+                    $_SESSION['name'] = $row['name'];
                     header("Location: admin_dashboard.php");
                 }
                 else{
